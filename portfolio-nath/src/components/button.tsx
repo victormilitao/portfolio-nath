280fbc248df709043ba17a755ef8abd251e0411d
Victor M Militao <victormilitao@gmail.com>
2025-03-08 23:03:06 -0300

    add some layout

--- portfolio-nath/src/components/button.tsx
+++ portfolio-nath/src/components/button.tsx
@@ -4,10 +4,10 @@ interface ButtonProps {
 
 export function Button(props: ButtonProps) {
   return (
-    <>
-      <button className='cursor-pointer max-w-fit py-2 px-6 border-[1px] border-solid border-zinc-900 rounded-3xl text-zinc-900'>
-        {props?.children}
+    
+      <button className='cursor-pointer max-w-fit py-1 px-6 border-[1px] border-solid border-zinc-900 rounded-3xl text-zinc-900 transition-transform duration-300 ease-in-out hover:bg-zinc-900 hover:text-white'>
+        <span className="text-sm">{props?.children}</span>
       </button>
-    </>
+    
   )
 }
--- portfolio-nath/src/pages/home.tsx
+++ portfolio-nath/src/pages/home.tsx
@@ -3,16 +3,18 @@ import { Button } from '../components/button'
 export function Home() {
   return (
     <div className='container w-full h-full flex justify-center items-center'>
-      <div className='content max-w-3xl flex flex-col gap-7 p-5'>
+      <div className='content max-w-[754px] flex flex-col gap-7 p-5'>
         <div className='profile flex gap-5'>
           <img
             className='w-[114px] rounded-full'
             src='./src/assets/profile.png'
             alt='Nathália Gonçalves'
           />
-          <div className='profile-info flex flex-col justify-center gap-1 text-amber-700'>
+          <div className='profile-info flex flex-col justify-center text-amber-700'>
             <p className='profile-name font-bold'>Nathália Gonçalves</p>
-            <p className='profile-job'>Product designer no ecossistema VR</p>
+            <p className='profile-job font-light mb-3'>
+              Product designer no ecossistema VR
+            </p>
             <div className='profile-buttons flex gap-2'>
               <Button>Currículo</Button>
               <Button>LinkedIn</Button>
@@ -20,16 +22,17 @@ export function Home() {
           </div>
         </div>
         <p className='text-3xl font-bold'>
-          Mais do que uma experiência intuitiva, crio produtos que geram impacto
-          real e mensurável.
+          Com análise de dados e design, transformo ideias em experiências
+          intuitivas que encantam.
         </p>
         <p className=''>
           Minha expertise está em criar experiências que fazem sentido para o
           negócio e para os usuários, através de pesquisas, construção e
           acompanhamento de métricas, discovery contínuo e análise de dados.
         </p>
-        <div className='project-card flex flex-auto rounded-xl shadow-lg'>
-          <div className='info w-[70%] p-4 flex flex-col gap-3'>
+
+        <div className='cursor-pointer project-card flex flex-auto rounded-xl shadow-[0_0_64px_rgba(0,0,0,0.1)] transition-transform duration-300 ease-in-out hover:scale-[101%]'>
+          <div className='info w-[70%] p-10 flex flex-col gap-3'>
             <p className='text-xl font-bold'>Escala - O Discovery</p>
             <p>
               Confira como realizei o discovery para a criação de uma
@@ -40,10 +43,35 @@ export function Home() {
             <Button>Abrir o projeto</Button>
           </div>
           <div className='work-schedule'>
-            <img className='w-full h-full object-cover' src='./src/assets/work-schedule-thumb.png' alt='Escala' />
+            <img
+              className='w-[293px] h-full object-cover'
+              src='./src/assets/work-schedule.png'
+              alt='Escala'
+            />
           </div>
         </div>
-        <div className='footer text-center'>
+
+        <div className='cursor-pointer project-card flex flex-auto rounded-xl shadow-[0_0_64px_rgba(0,0,0,0.1)] transition-transform duration-300 ease-in-out hover:scale-[101%]'>
+          <div className='info w-[70%] p-10 flex flex-col gap-3'>
+            <p className='text-xl font-bold'>Medindo o sucesso de um produto</p>
+            <p>
+              Justifiquei a necessidade de melhorias com base em métricas de
+              sucesso e gerei impacto positivo para o negócio e para os
+              usuários.
+            </p>
+
+            <Button>Abrir o projeto</Button>
+          </div>
+          <div className='metrics'>
+            <img
+              className='w-[293px] h-full object-cover'
+              src='./src/assets/metrics.png'
+              alt='Escala'
+            />
+          </div>
+        </div>
+
+        <div className='footer text-center text-sm'>
           <span>Confira mais projetos aqui em breve :)</span>
         </div>
       </div>
