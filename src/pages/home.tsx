95d90bc3c5a8d86d610cae556496e6b47c2af9a6
Victor Militão <victormilitao@gmail.com>
2025-03-09 23:18:29 -0300

    add spacing on elements

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -9,7 +9,7 @@ export function Home() {
       <div className='content max-w-[754px] flex flex-col gap-5 p-5'>
         <div className='profile flex gap-5'>
           <img
-            className='w-16 h-fit sm:w-[114px] rounded-full'
+            className='w-20 h-full sm:w-[114px] rounded-full'
             src={profile}
             alt='Nathália Gonçalves'
           />
@@ -18,24 +18,30 @@ export function Home() {
             <p className='profile-job font-light mb-3'>
               Product designer no ecossistema VR
             </p>
-            <div className='profile-buttons flex gap-2'>
+            <div className='hidden sm:flex gap-2'>
               <Button>Currículo</Button>
               <Button>LinkedIn</Button>
             </div>
           </div>
         </div>
+
+        <div className='sm:hidden min-w-full flex gap-2'>
+          <Button>Currículo</Button>
+          <Button>LinkedIn</Button>
+        </div>
+
         <p className='text-3xl font-bold'>
           Com análise de dados e design, transformo ideias em experiências
           intuitivas que encantam.
-        </p>
-        <p className=''>
-          Minha expertise está em criar experiências que fazem sentido para o
-          negócio e para os usuários, através de pesquisas, construção e
-          acompanhamento de métricas, discovery contínuo e análise de dados.
+          <p className='text-lg font-normal mt-3'>
+            Minha expertise está em criar experiências que fazem sentido para o
+            negócio e para os usuários, através de pesquisas, construção e
+            acompanhamento de métricas, discovery contínuo e análise de dados.
+          </p>
         </p>
 
-        <div className='flex flex-col-reverse sm:flex-row sm:flex-auto overflow-hidden cursor-pointer rounded-xl text-primary-2 shadow-[0_0_64px_rgba(0,0,0,0.1)] transition-transform duration-300 ease-in-out hover:scale-[101%]'>
-          <div className='info w-[70%] p-10 flex flex-col gap-3'>
+        <div className='mt-2 flex flex-col-reverse sm:flex-row sm:flex-auto overflow-hidden cursor-pointer rounded-xl text-primary-2 shadow-[0_0_64px_rgba(0,0,0,0.1)] transition-transform duration-300 ease-in-out hover:scale-[101%]'>
+          <div className='sm:w-[70%] p-10 flex flex-col gap-2'>
             <p className='text-xl font-bold'>Escala - O Discovery</p>
             <p>
               Confira como realizei o discovery para a criação de uma
@@ -43,7 +49,9 @@ export function Home() {
               ponto.
             </p>
 
-            <Button>Abrir o projeto</Button>
+            <div className='mt-2'>
+              <Button>Abrir o projeto</Button>
+            </div>
           </div>
           <div className='work-schedule'>
             <img
@@ -54,8 +62,8 @@ export function Home() {
           </div>
         </div>
 
-        <div className='flex flex-col-reverse sm:flex-row sm:flex-auto overflow-hidden cursor-pointer rounded-xl text-primary-2 shadow-[0_0_64px_rgba(0,0,0,0.1)] transition-transform duration-300 ease-in-out hover:scale-[101%]'>
-          <div className='info w-[70%] p-10 flex flex-col gap-3'>
+        <div className='mt-2 flex flex-col-reverse sm:flex-row sm:flex-auto overflow-hidden cursor-pointer rounded-xl text-primary-2 shadow-[0_0_64px_rgba(0,0,0,0.1)] transition-transform duration-300 ease-in-out hover:scale-[101%]'>
+          <div className='sm:w-[70%] p-10 flex flex-col gap-3'>
             <p className='text-xl font-bold'>Medindo o sucesso de um produto</p>
             <p>
               Justifiquei a necessidade de melhorias com base em métricas de
@@ -63,7 +71,9 @@ export function Home() {
               usuários.
             </p>
 
-            <Button>Abrir o projeto</Button>
+            <div className='mt-2'>
+              <Button>Abrir o projeto</Button>
+            </div>
           </div>
           <div className='metrics'>
             <img
