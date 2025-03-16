54c08b2c8da0ef614ee03f4b367905e4c8305169
Victor Militão <victormilitao@gmail.com>
2025-03-16 19:42:27 -0300

    add active effect to project

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -11,7 +11,7 @@ type ProjectProps = {
 export const Project = ({ img, title, subtitle, url }: ProjectProps) => {
   return (
     <Link to={url}>
-      <div className='mt-2 flex flex-col-reverse sm:flex-row sm:flex-auto overflow-hidden cursor-pointer rounded-xl text-primary-2 shadow-[0_0_64px_rgba(0,0,0,0.1)] transition-transform duration-300 ease-in-out hover:scale-[101%]'>
+      <div className='mt-2 flex flex-col-reverse sm:flex-row sm:flex-auto overflow-hidden cursor-pointer rounded-xl text-primary-2 shadow-[0_0_64px_rgba(0,0,0,0.1)] transition-transform duration-300 ease-in-out hover:scale-[101%] active:scale-[101%]'>
         <div className='sm:w-[70%] p-10 flex flex-col gap-2'>
           <p className='text-xl font-bold'>{title}</p>
           <p>{subtitle}</p>
